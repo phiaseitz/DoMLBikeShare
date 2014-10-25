86cc44357000e334c81bf4a27d8471d81ea60329
phiaseitz <sophia.seitz@students.olin.edu>
2014-10-25 12:44:40 -0400

    Working csv reading file

--- import_data.py
+++ import_data.py
@@ -1,25 +1,42 @@
 import csv
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, linear_model
 
-date = []
-time = []
-season = []
-holiday = []
-workingday = []
-weather = []
-temp = []
-atemp = []
-humidity = []
-windspeed = []
-casual = []
-registered = []
-total = []
+# date = []
+# time = []
+# season = []
+# holiday = []
+# workingday = []
+# weather = []
+# temp = []
+# atemp = []
+# humidity = []
+# windspeed = []
+# casual = []
+# registered = []
+# total = []
 
-with open('train.csv', 'rb') as csvfile:
-	bikereader = csv.reader(csvfile, delimiter=',', quotechar='|')
-  	for row in bikereader:
-  		for column in row:
-  			
-  		X.append[]
+def read_csv(file_path, has_header = True):
+    with open(file_path) as f:
+        if has_header: f.readline()
+        data = []
+        for line in f:
+            line = line.strip().split(",")
+
+            date = line[0][0:line[0].index(' ')]
+            date = date.translate(None, '-')
+            time = line[0][line[0].index(' '): len(line[0])]
+            time = time[0:2]
+
+            line.pop(0)
+            line.insert(0, date)
+            line.insert(1, time)
+            data.append([float(x) for x in line])
+            npdata = np.array(data)
+    return npdata  
+
+def main ():
+    data = read_csv('train.csv')
+    print (data)
+
+if __name__ == '__main__':
+    main()

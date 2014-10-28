715bd3cb363d51160751253deb691fd43761046e
Shrinidhi Thirumalai <shrinidhit@Shrinidhi.(none)>
2014-10-28 19:38:28 -0400

    Adding function to convert day, month, and year to a single time value. May or may not be needed

--- logistic_regress.py
+++ logistic_regress.py
@@ -6,17 +6,24 @@ import import_data
 #Import the csv
 importedData = import_data.read_csv('train.csv')
 
+def convert_times(data):
+	#year, month, day
+	[year, month, day] = data[0:3]
+	days_in_month = [1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31]
+	time = (year - 2011)*365 + month*days_in_month[int(month)] + day
+	return [time, data[3:len(data)]]
+
 #Split into inputs and outputs
 X = importedData[:, 0: importedData.shape[1] - 3]
 y = importedData[:, importedData.shape[1] - 1]
 
 #wX = np.split(X, np.where(X[:,3] == 1.) [0][0:])
 #split into working day and non-workind day data sets so that we can learn two different models
-wX = X[X[:,4] == 1.]
-hX = X[X[:,4] != 1.]
+wX = X[X[:,6] == 1.]
+hX = X[X[:,6] != 1.]
 
-wy = y[X[:,4] == 1.]
-hy = y[X[:,4] != 1.]
+wy = y[X[:,6] == 1.]
+hy = y[X[:,6] != 1.]
 
 wX_train , wX_test, wy_train, wy_test = cross_validation.train_test_split(wX, wy, test_size=.5)
 hX_train , hX_test, hy_train, hy_test = cross_validation.train_test_split(hX, hy, test_size=.5)
@@ -31,3 +38,4 @@ print (wy)
 # print (y)
 
 plt.plot(wX[:,0],wy)
+plt.show()
